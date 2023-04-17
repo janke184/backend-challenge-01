a0b93b24eecc6db121980edf6aed6bf8b15aa07b
Giancarlo Sereni <giancarlosereni@gmail.com>
2023-04-17 20:29:15 -0300

    Validations

--- src/clientes/dtos/create-cliente.dto.ts
+++ src/clientes/dtos/create-cliente.dto.ts
@@ -1,13 +1,19 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsDate, IsNotEmpty, IsString } from "class-validator";
 
 export class CreaClienteDto {
 
+    @IsString()
+    @IsNotEmpty()
     @ApiProperty()
     nombre: string;
 
+    @IsString()
+    @IsNotEmpty()
     @ApiProperty()
     apellido: string;
     
+    @IsNotEmpty()
     @ApiProperty()
     fechaNacimiento: Date;
 
